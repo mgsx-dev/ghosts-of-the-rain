eca145f88c99d4838042b5c132be592380cb2afc
Germain AUBERT <germain.aubert@gmail.com>
2018-03-31 09:00:33 +0200

    init from template

---
+++ android/src/net/mgsx/rainyday/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.mgsx.rainyday;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.mgsx.rainyday.RainyDay;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RainyDay(), config);
+	}
+}
