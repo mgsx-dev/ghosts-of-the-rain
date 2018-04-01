f9ebc30459dfcc4da64f93e6fcab7e68b54d489a
Germain AUBERT <germain.aubert@gmail.com>
2018-04-02 00:21:50 +0200

    distribution

--- desktop/src/net/mgsx/rainyday/DesktopLauncher.java
+++ desktop/src/net/mgsx/rainyday/DesktopLauncher.java
@@ -2,11 +2,11 @@ package net.mgsx.rainyday;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import net.mgsx.rainyday.RainyDay;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Ghosts of the rain";
 		new LwjglApplication(new RainyDay(), config);
 	}
 }
